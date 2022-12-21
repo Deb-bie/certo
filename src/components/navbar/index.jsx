c1e40cd1b02d88e55d12d41f38de71dfbf925631
Debbie <asamoahjaeldeborah@gmail.com>
2022-12-21 11:11:10 +0000

    Initial commit

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div>Logo</div>
+
+            <div>items</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
