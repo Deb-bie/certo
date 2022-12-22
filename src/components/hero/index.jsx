c2a032783b06b733c027dd0f69d4e068395414f9
Debbie <asamoahjaeldeborah@gmail.com>
2022-12-22 07:32:32 +0000

    First section

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -6,8 +6,20 @@ import Cnbc from "../../assets/images/cnbc.png"
 import Wired from "../../assets/images/wired.png"
 import Zd from "../../assets/images/zd.png"
 import Readers from "../../assets/images/readers.png"
+import Yellow from "../../assets/images/yellow.png"
+import Girl from "../../assets/images/girl.png"
+// import Readers from "../../assets/images/readers.png"
+// import Readers from "../../assets/images/readers.png"
+// import Readers from "../../assets/images/readers.png"
+// import Readers from "../../assets/images/readers.png"
+// import Readers from "../../assets/images/readers.png"
+// import Readers from "../../assets/images/readers.png"
+// import Readers from "../../assets/images/readers.png"
+// import Readers from "../../assets/images/readers.png"
+// import Readers from "../../assets/images/readers.png"
 
 
+import './hero.css'
 
 
 const Hero = () => {
@@ -41,6 +53,30 @@ const Hero = () => {
             <img src={Wired} alt="wired" className='w-[130px] h-auto object-contain' />
             <img src={Zd} alt="zd" className='w-[69px] h-auto object-contain' />
             <img src={Readers} alt="readers" className='w-[130px] h-auto object-contain' />
+        </div>
+
+        <div className='w-[100%] h-[100%] flex 4xs:flex-col md:flex-row justify-center content-center items-center 4xs:px-6 md:px-32 py-12 gap-y-12 '>
+            <div className='4xs:w-[100%] md:w-[40%] relative '>
+                <img src={Yellow} alt="yellow" className="w-[80%] h-auto absolute object-contain " />
+                <img src={Girl} alt="girl" className="w-[80%] h-auto relative object-contain ml-6 -mt-6" />
+                {/* <div className="hero relative object-contain ml-4 -mt-4 bg-[#404040]" ></div> */}
+            </div>
+
+            <div className='4xs:w-[100%] md:w-[50%] h-[100%] flex flex-col justify-between '>
+                <h2 className='text-[#181818] text-xl font-[700] leading-10  '>At Certo, mobile security is not an afterthought, it’s what we do.</h2>
+                <p className='text-[#616161] font-[400] py-8  '>With years of experience in mobile security and spyware detection, our products have helped countless people safeguard their devices and find peace of mind.</p>
+
+                <div className='w-[100%] flex flex-row flex-wrap gap-6'>
+                    <button className='rounded-[10px] py-2 px-6 ml-4 bg-[#02033B] text-white text-base 4xs:w-[100%] sm:w-auto  '>
+                        Get for iOS
+                    </button>
+
+                    <button className='rounded-[10px] py-2 px-6 ml-4 text-[#02033B] border-[1px] border-[#02033B] text-base 4xs:w-[100%] sm:w-auto   '>
+                        Get for Android
+                    </button>
+                </div>
+            </div>
+
         </div>
     </div>
   )
