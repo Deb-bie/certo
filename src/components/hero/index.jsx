ce2ed0e9a5a94c079f8977225c65acd0712f18f1
Debbie <asamoahjaeldeborah@gmail.com>
2022-12-22 08:34:40 +0000

    Worked on the second section

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -8,7 +8,7 @@ import Zd from "../../assets/images/zd.png"
 import Readers from "../../assets/images/readers.png"
 import Yellow from "../../assets/images/yellow.png"
 import Girl from "../../assets/images/girl.png"
-// import Readers from "../../assets/images/readers.png"
+import Eye from "../../assets/images/eye.png"
 // import Readers from "../../assets/images/readers.png"
 // import Readers from "../../assets/images/readers.png"
 // import Readers from "../../assets/images/readers.png"
@@ -18,6 +18,7 @@ import Girl from "../../assets/images/girl.png"
 // import Readers from "../../assets/images/readers.png"
 // import Readers from "../../assets/images/readers.png"
 
+import { Freedom } from '../../data/data'
 
 import './hero.css'
 
@@ -76,7 +77,34 @@ const Hero = () => {
                     </button>
                 </div>
             </div>
+        </div>
 
+        <div className='w-[100%] h-auto bg-[#F8F9FD] flex flex-col content-center items-center 4xs:px-6 sm:px-20 md:px-40 py-12 my-12 '>
+            <h1 className='text-xl font-[700] text-[#181818]'>Get your freedom back, stop mobile spyware today</h1>
+            <div className='w-[100%] py-12 grid 4xs:grid-cols-1 sm:grid-cols-2 gap-x-[70px]'>
+
+                {
+                    Freedom.map((item, id) => (
+                        <div key={id} className='flex flex-col gap-y-4 pb-24'>
+                            <span className='flex flex-row items-center gap-x-2'>
+                                <img src={item.image} alt="eye" className='object-contain bg-[ #FFC247]' />
+                                <p className='font-[700]'>{item.title}</p>
+                            </span>
+                            <p>{item.desc}</p>
+                        </div>
+                    ))
+                }
+            </div>
+
+            <div className='w-[100%] flex flex-row items-center content-center justify-center flex-wrap gap-6'>
+                <button className='rounded-[10px] py-2 px-6 ml-4 bg-[#02033B] text-white text-base 4xs:w-[100%] sm:w-auto  '>
+                    Get for iOS
+                </button>
+
+                <button className='rounded-[10px] py-2 px-6 ml-4 text-[#02033B] border-[1px] border-[#02033B] text-base 4xs:w-[100%] sm:w-auto   '>
+                    Get for Android
+                </button>
+            </div>
         </div>
     </div>
   )
---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+import Eye from "../assets/images/eye.png"
+import Key from "../assets/images/key.png"
+import Analytics from "../assets/images/analytics.png"
+import Battery from "../assets/images/battery.png"
+import Threat from "../assets/images/threat.png"
+import Easy from "../assets/images/easy.png"
+
+
+
+
+export const Freedom = [
+    {
+        image: Eye,
+        title: "Spyware detection",
+        desc: "Our advanced spyware detection engine can identify if a device contains spyware or bugging software."
+    },
+    {
+        image: Key,
+        title: "Keylogger detection",
+        desc: "Find malicious keyboards installed on your device that could allow someone to record things you type (e.g. passwords)."
+    },
+    {
+        image: Analytics,
+        title: "Find tracking apps",
+        desc: "Check which apps can access your location, microphone or camera. Get alerted if a known tracking app is installed."
+    },
+    {
+        image: Battery,
+        title: "OS integrity check",
+        desc: "Analyze your operating system for signs of tampering that could compromise security, such as Jailbreaking."
+    },
+    {
+        image: Threat,
+        title: "Threat removal",
+        desc: "Our intelligent scan will either safely remove threats for you or provide easy-to-follow instructions."
+    },
+    {
+        image: Easy,
+        title: "Easy to use",
+        desc: "We create easy to use apps that can check your device for vulnerabilities in a matter of minutes."
+    }
+]
